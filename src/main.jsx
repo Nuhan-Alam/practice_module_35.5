89d6529cafc58b9a5a404fabbcf92f6bb53d4b18
Nuhan Alam <nuhanalam@gmail.com>
2025-09-01 14:08:29 +0600

    import name fixed

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router";
 import './index.css'
-import AppRoutes from './routes/approutes.jsx';
+import AppRoutes from './routes/Approutes.jsx';
 import { AuthProvider } from './contexts/AuthContext.jsx';
 
 createRoot(document.getElementById('root')).render(
